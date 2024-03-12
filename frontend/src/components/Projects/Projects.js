45ddc5693d1a5d1f40f84249f2bb5f3be2e0b34f
Armaan Seth <seth.armaanseth@gmail.com>
2024-03-12 16:48:33 +0530

    added cards

--- frontend/src/components/Projects/Projects.js
+++ frontend/src/components/Projects/Projects.js
@@ -1,64 +1,26 @@
 import './Projects.css'
+import project_list from './project_list';
+function createCard(project){
+    return(
+        <div className='card'>
+            <h3>{project.heading}</h3>
+            <div className='card-image'>
+                <img src={project.img}/>
+            </div>
+            <p>{project.des}</p>
+            <div className='card-footer'>
+                <button className='card-btn' href={project.glink}><i class="fa-brands fa-github"></i> GitHub</button>
+                <button className='card-btn' href={project.wlink}><i class="fa-solid fa-globe"></i> WebLink</button>
+            </div>
+        </div>
+    )
+}
 function Projects() {
     return (
       <div className="Projects">       
         <h1>Projects</h1>
         <div className='projects-container'>
-            <div className='card'>
-                <h3>MineBot</h3>
-                <div className='card-image'>
-                    <img src='https://raw.githubusercontent.com/ArmaanSeth/Images/main/MineBot1.png'/>
-                </div>
-                <p>The primary purpose of MineBot.In is to provide accurate and reliable information regarding mining acts, rules, and regulations in India. The project leverages advanced natural language processing techniques to understand user queries and generate contextually relevant responses. It serves as a valuable resource for individuals, businesses, and organizations seeking information on mining-related legal frameworks in India.</p>
-                <div className='card-footer'>
-                    <button className='card-btn'><i class="fa-brands fa-github"></i> GitHub</button>
-                    <button className='card-btn'><i class="fa-solid fa-globe"></i> WebLink</button>
-                </div>
-            </div>
-            <div className='card'>
-                <h3>MineBot</h3>
-                <div className='card-image'>
-                    <img src='https://raw.githubusercontent.com/ArmaanSeth/Images/main/MineBot1.png'/>
-                </div>
-                <p>The primary purpose of MineBot.In is to provide accurate and reliable information regarding mining acts, rules, and regulations in India. The project leverages advanced natural language processing techniques to understand user queries and generate contextually relevant responses. It serves as a valuable resource for individuals, businesses, and organizations seeking information on mining-related legal frameworks in India.</p>
-                <div className='card-footer'>
-                    <button className='card-btn'><i class="fa-brands fa-github"></i> GitHub</button>
-                    <button className='card-btn'><i class="fa-solid fa-globe"></i> WebLink</button>
-                </div>
-            </div>
-            <div className='card'>
-                <h3>MineBot</h3>
-                <div className='card-image'>
-                    <img src='https://raw.githubusercontent.com/ArmaanSeth/Images/main/MineBot1.png'/>
-                </div>
-                <p>iques to understand user queries and generate contextually relevant responses. It serves as a valuable resource for individuals, businesses, and organizations seeking information on mining-related legal frameworks in India.</p>
-                <div className='card-footer'>
-                    <button className='card-btn'><i class="fa-brands fa-github"></i> GitHub</button>
-                    <button className='card-btn'><i class="fa-solid fa-globe"></i> WebLink</button>
-                </div>
-            </div> 
-            <div className='card'>
-                <h3>MineBot</h3>
-                <div className='card-image'>
-                    <img src='https://raw.githubusercontent.com/ArmaanSeth/Images/main/MineBot1.png'/>
-                </div>
-                <p>iques to understand user queries and generate contextually relevant responses. It serves as a valuable resource for individuals, businesses, and organizations seeking information on mining-related legal frameworks in India.</p>
-                <div className='card-footer'>
-                    <button className='card-btn'><i class="fa-brands fa-github"></i> GitHub</button>
-                    <button className='card-btn'><i class="fa-solid fa-globe"></i> WebLink</button>
-                </div>
-            </div> 
-            <div className='card'>
-                <h3>MineBot</h3>
-                <div className='card-image'>
-                    <img src='https://raw.githubusercontent.com/ArmaanSeth/Images/main/MineBot1.png'/>
-                </div>
-                <p>iques to understand user queries and generate contextually relevant responses. It serves as a valuable resource for individuals, businesses, and organizations seeking information on mining-related legal frameworks in India.</p>
-                <div className='card-footer'>
-                    <button className='card-btn'><i class="fa-brands fa-github"></i> GitHub</button>
-                    <button className='card-btn'><i class="fa-solid fa-globe"></i> WebLink</button>
-                </div>
-            </div> 
+            {project_list.map(createCard)}
         </div>
       </div>
       
--- frontend/src/components/Projects/project_list.js
+++ frontend/src/components/Projects/project_list.js
@@ -2,7 +2,7 @@ const project_list=[
     {
         heading:"MineBot",
         img:"https://raw.githubusercontent.com/ArmaanSeth/Images/main/MineBot1.png",
-        des:"The primary purpose of MineBot.In is to provide accurate and reliable information regarding mining acts, rules, and regulations in India. The project leverages advanced natural language processing techniques to understand user queries and generate contextually relevant responses. It serves as a valuable resource for individuals, businesses, and organizations seeking information on mining-related legal frameworks in India.",
+        desc:"The primary purpose of MineBot.In is to provide accurate and reliable information regarding mining acts, rules, and regulations in India. The project leverages advanced natural language processing techniques to understand user queries and generate contextually relevant responses. It serves as a valuable resource for individuals, businesses, and organizations seeking information on mining-related legal frameworks in India.",
         glink:"https://github.com/ArmaanSeth/MineBot",
         wlink:""
     }
